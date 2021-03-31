0f683a373cc95cb9fa8f4725572a39925c75c96a
tegarerik <tegarerik04@gmail.com>
2021-03-31 20:35:32 +0700

    menambahkan tugas overloading method ke github

---
+++ Overloading Method/Overloading Method/Numbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Method
+{
+    class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            return (new int[] { number1, number2 }).Min();
+        }
+        public float FindMinimum(int number1, int number2, float number3)
+        {
+            return (new float[] { number1, number2, number3 }).Min();
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            return (new int[] { number1, number2 }).Max();
+        }
+        public float FindMaximum(int number1, int number2, float number3)
+        {
+            return (new float[] { number1, number2, number2, number3 }).Max();
+        }
+            
+    }
+}
---
+++ Overloading Method/Overloading Method/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using System.Windows.Forms;
+
+namespace Overloading_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+            
+            Console.WriteLine($"Minimum #1 :{numbers.FindMinimum(3, 1)}");
+            Console.WriteLine($"Minimum #2 : {numbers.FindMinimum(3, 2, 4)}");
+            Console.WriteLine();
+            Console.WriteLine($"Maximum #1 : {numbers.FindMaximum(3, 1)}");
+            Console.WriteLine($"Maximum #2 : {numbers.FindMaximum(3, 2, 4)}");
+            Console.ReadKey();
+        }
+    }
+}
